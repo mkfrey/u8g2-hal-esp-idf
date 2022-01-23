775ca5227457da44818290d3b4e27f123e77abd4
Michal Dvorak <mikee2185@gmail.com>
2022-01-23 19:06:17 +0100

    chore(example): fixed I2C example

--- example/main/u8g2_hal_example_main.c
+++ example/main/u8g2_hal_example_main.c
@@ -6,7 +6,7 @@ void task_test_SSD1306(void *ignore);
 void task_test_SSD1306i2c(void *ignore);
 
 _Noreturn void app_main() {
-#if EXAMPLE_INTERFACE_I2C
+#if CONFIG_EXAMPLE_INTERFACE_I2C
     xTaskCreate(task_test_SSD1306i2c, "test", 4000, NULL, tskIDLE_PRIORITY, NULL);
 #else
     xTaskCreate(task_test_SSD1306, "test", 4000, NULL, tskIDLE_PRIORITY, NULL);
